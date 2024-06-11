522cb173ef214789433983e17181a944a9a6cf4c
Pedro Luiz <pedroluiz000111@gmail.com>
2024-06-11 20:30:44 -0300

    feat(p2-pdm): botão acrescenta 5 imagens a cada clique no botão

--- busca_gato/app/index.tsx
+++ busca_gato/app/index.tsx
@@ -1,62 +1,63 @@
 import React, { useState } from 'react';
 import {
-  Alert,
-  FlatList,
-  Pressable,
-  StyleSheet,
-  Text,
-  TextInput,
-  ToastAndroid,
-  View,
+ Alert,
+ FlatList,
+ Pressable,
+ StyleSheet,
+ Text,
+ TextInput,
+ ToastAndroid,
+ View,
 } from 'react-native';
 import CatImage from './CatImage';
  
 export default function App() {
-  const [buttonColor, setButtonColor] = useState('#011F51');
-  const [showCatImages, setShowCatImages] = useState(false);
+ const [buttonColor, setButtonColor] = useState('#011F51');
+ const [catImages, setCatImages] = useState([]);
  
-  const handleButtonPress = () => {
-    setShowCatImages(true);
-  };
+ const handleButtonPress = () => {
+   const newCatImages = [...catImages];
+   for (let i = 0; i < 5; i++) {
+     newCatImages.push({ id: newCatImages.length + i, uri: "https://api.thecatapi.com/v1/images/search?limit=1" });
+   }
+   setCatImages(newCatImages);
+ };
  
-  return (
-    <View style={styles.container}>
-      <Pressable
-        onPressIn={() => setButtonColor('#018C9B')}
-        onPressOut={() => setButtonColor('#011F51')}
-        onPress={handleButtonPress}
-        style={[styles.button, { backgroundColor: buttonColor }]}
-      >
-        <Text style={styles.buttonText}>{`Buscar Gatos`}</Text>
-      </Pressable>
-      {showCatImages && (
-        <FlatList
-          data={[...Array(5).keys()]}
-          renderItem={({ item }) => <CatImage key={item} />}
-          keyExtractor={(item) => item.toString()}
-          horizontal
-        />
-      )}
-    </View>
-  );
+ return (
+   <View style={styles.container}>
+     <Pressable
+       onPressIn={() => setButtonColor('#018C9B')}
+       onPressOut={() => setButtonColor('#011F51')}
+       onPress={handleButtonPress}
+       style={[styles.button, { backgroundColor: buttonColor }]}
+     >
+       <Text style={styles.buttonText}>{`Buscar Gatos`}</Text>
+     </Pressable>
+     <FlatList
+       data={catImages}
+       renderItem={({ item }) => <CatImage uri={item.uri} />}
+       keyExtractor={(item) => item.id.toString()}
+     />
+   </View>
+ );
 }
  
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#6EADA2',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 40,
-  },
-  button: {
-    width: '80%',
-    padding: 8,
-    borderRadius: 4,
-    marginBottom: 4,
-  },
-  buttonText: {
-    color: 'white',
-    textAlign: 'center',
-  },
+ container: {
+   flex: 1,
+   backgroundColor: '#6EADA2',
+   alignItems: 'center',
+   justifyContent: 'center',
+   paddingVertical: 40,
+ },
+ button: {
+   width: '80%',
+   padding: 8,
+   borderRadius: 4,
+   marginBottom: 4,
+ },
+ buttonText: {
+   color: 'white',
+   textAlign: 'center',
+ },
 });
